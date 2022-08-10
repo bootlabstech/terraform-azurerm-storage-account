3a5791ec4e3016cf5490f58b655221a0b5a3ab55
DivyadharshiniS <sdivya-cont@mahindra.com>
2022-08-10 14:23:44 +0530

    initial commit

---
+++ main.tf
@@ -0,0 +1,54 @@
+# Create a resource group
+data "azurerm_resource_group" "bootlab_rg" {
+  name     = var.group_name
+}
+resource "random_string" "storage_account_random_suffix" {
+  length  = 8
+  special = false
+  lower   = true
+  upper   = false
+}
+
+resource "azurerm_storage_account" "assets" {
+  name                            = "${var.storage_account_name}${random_string.storage_account_random_suffix.id}"
+  resource_group_name             = data.azurerm_resource_group.bootlab_rg.name
+  location                        = data.azurerm_resource_group.bootlab_rg.location
+  account_tier                    = "Standard"
+  account_replication_type        = var.file_storage_account_replication_type
+  enable_https_traffic_only       = true
+  allow_nested_items_to_be_public = true
+
+  lifecycle {
+    ignore_changes = [
+      # Ignore changes to tags, e.g. because a management agent
+      # updates these based on some ruleset managed elsewhere.
+      tags,
+    ]
+  }
+}
+
+resource "azurerm_storage_share" "storage_share" {
+  for_each             = { for storage in var.file_storage_list : storage.name => storage }
+  name                 = each.key
+  storage_account_name = azurerm_storage_account.assets.name
+  quota                = each.value.quota
+}
+
+resource "azurerm_storage_container" "storage_container" {
+  for_each              = { for storage in var.container_set : storage.name => storage }
+  name                  = each.key
+  storage_account_name  = azurerm_storage_account.assets.name
+  container_access_type = each.value.access_type
+}
+
+resource "azurerm_storage_queue" "queues" {
+  for_each             = { for storage in var.queues_set : storage.name => storage }
+  name                 = each.key
+  storage_account_name = azurerm_storage_account.assets.name
+}
+
+resource "azurerm_storage_table" "tables" {
+  for_each             = { for storage in var.tables_set : storage.name => storage }
+  name                 = each.key
+  storage_account_name = azurerm_storage_account.assets.name
+}
---
+++ variables.tf
@@ -0,0 +1,38 @@
+variable "group_name" {
+  type        = string
+  description = "The Name which should be used for this Resource Group. Changing this forces a new Resource Group to be created."
+}
+
+variable "storage_account_name" {
+  type        = string
+  description = "name of the storage account"
+}
+
+variable "file_storage_account_replication_type" {
+  type        = string
+  description = "replication type of file storage, LRS in uat and GRS in prod"
+}
+
+variable "container_set" {
+  description = "List of containers to create and their access levels."
+  type        = list(object({ name = string, access_type = string }))
+  default     = []
+}
+
+variable "file_storage_list" {
+  description = "List of containers to create and their access levels."
+  type        = list(object({ name = string, quota = number }))
+  default     = []
+}
+
+variable "queues_set" {
+  description = "List of storages queues"
+  type        = list(object({ name = string}))
+  default     = []
+}
+
+variable "tables_set" {
+  description = "List of storage tables."
+  type        = list(object({ name = string}))
+  default     = []
+}
