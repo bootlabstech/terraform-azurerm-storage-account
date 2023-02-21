d766208c50c72c51519c8a182ab71dba52958c28
naveenkumar sivarajan <sivarajan.naveenkumar@mahindra.com>
2023-02-22 01:00:20 +0530

    test

--- main.tf
+++ main.tf
@@ -18,6 +18,24 @@ resource "azurerm_storage_account" "assets" {
   enable_https_traffic_only         = true
   allow_nested_items_to_be_public   = true
   infrastructure_encryption_enabled = true
+  shared_access_key_enabled         = true
+
+  network_rules {
+    default_action = "Deny"
+    bypass         = "AzureServices"
+  }
+
+  blob_properties {
+    delete_retention_policy {
+      enabled = true
+      days    = 7
+    }
+
+    container_delete_retention_policy {
+      enabled = true
+      days    = 7
+    }
+  }
 
   lifecycle {
     ignore_changes = [
@@ -28,28 +46,29 @@ resource "azurerm_storage_account" "assets" {
   }
 }
 
-resource "azurerm_storage_share" "storage_share" {
-  for_each             = { for storage in var.file_storage_list : storage.name => storage }
-  name                 = each.key
-  storage_account_name = azurerm_storage_account.assets.name
-  quota                = each.value.quota
-}
 
-resource "azurerm_storage_container" "storage_container" {
-  for_each              = { for storage in var.container_set : storage.name => storage }
-  name                  = each.key
-  storage_account_name  = azurerm_storage_account.assets.name
-  container_access_type = "private"
-}
+#resource "azurerm_storage_share" "storage_share" {
+#  for_each             = { for storage in var.file_storage_list : storage.name => storage }
+#  name                 = each.key
+#  storage_account_name = azurerm_storage_account.assets.name
+#  quota                = each.value.quota
+#}
 
-resource "azurerm_storage_queue" "queues" {
-  for_each             = { for storage in var.queues_set : storage.name => storage }
-  name                 = each.key
-  storage_account_name = azurerm_storage_account.assets.name
-}
+#resource "azurerm_storage_container" "storage_container" {
+#  for_each              = { for storage in var.container_set : storage.name => storage }
+#  name                  = each.key
+#  storage_account_name  = azurerm_storage_account.assets.name
+#  container_access_type = "private"
+#}
 
-resource "azurerm_storage_table" "tables" {
-  for_each             = { for storage in var.tables_set : storage.name => storage }
-  name                 = each.key
-  storage_account_name = azurerm_storage_account.assets.name
-}
+#resource "azurerm_storage_queue" "queues" {
+#  for_each             = { for storage in var.queues_set : storage.name => storage }
+#  name                 = each.key
+#  storage_account_name = azurerm_storage_account.assets.name
+#}
+
+#resource "azurerm_storage_table" "tables" {
+#  for_each             = { for storage in var.tables_set : storage.name => storage }
+#  name                 = each.key
+#  storage_account_name = azurerm_storage_account.assets.name
+#}
