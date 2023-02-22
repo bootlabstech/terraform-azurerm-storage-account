5239d728667161e01feb3fead964ab804ed366eb
naveenkumar sivarajan <sivarajan.naveenkumar@mahindra.com>
2023-02-22 16:59:56 +0530

    test

--- main.tf
+++ main.tf
@@ -13,7 +13,8 @@ resource "azurerm_storage_account" "assets" {
   name                              = "${var.storage_account_name}${random_string.storage_account_random_suffix.id}"
   resource_group_name               = data.azurerm_resource_group.bootlab_rg.name
   location                          = data.azurerm_resource_group.bootlab_rg.location
-  account_tier                      = "Standard"
+  account_kind                      = "StorageV2"
+  account_tier                      = "Premium"
   account_replication_type          = var.file_storage_account_replication_type
   enable_https_traffic_only         = true
   allow_nested_items_to_be_public   = true
