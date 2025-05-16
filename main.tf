d2f9eed8268844f27107f5f4abdb4cb215ab2d80
Bhargava Naidu <bhargava@bootlabstech.com>
2025-05-16 12:25:31 +0530

    updated the main and variables, provider

--- main.tf
+++ main.tf
@@ -1,25 +1,39 @@
 resource "azurerm_storage_account" "example" {
-  name                             = var.name
-  resource_group_name              = var.resource_group_name
-  location                         = var.location
-  account_tier                     = var.account_tier
-  account_replication_type         = var.account_replication_type
-  min_tls_version                  = var.min_tls_version
-  cross_tenant_replication_enabled = var.cross_tenant_replication_enabled
-  allow_nested_items_to_be_public  = var.allow_nested_items_to_be_public
-  shared_access_key_enabled        = var.shared_access_key_enabled
-  public_network_access_enabled    = var.public_network_access_enabled
-  enable_https_traffic_only        = var.enable_https_traffic_only
-  account_kind                     = var.account_kind
-  identity {
-    type = var.type
-  }
-  network_rules {
-    default_action = "Deny"
-    ip_rules       = ["100.0.0.1"]
-    bypass =  ["AzureServices" ]
-  }
+  name                                = var.name
+  resource_group_name                 = var.resource_group_name
+  location                            = var.location
+  account_tier                        = var.account_tier
+  account_kind                        = var.account_kind
+  account_replication_type            = var.account_replication_type
+  cross_tenant_replication_enabled    = var.cross_tenant_replication_enabled
+  access_tier                         = var.access_tier
+  enable_https_traffic_only           = var.enable_https_traffic_only
+  min_tls_version                     = var.min_tls_version
+  allow_nested_items_to_be_public     = var.allow_nested_items_to_be_public
+  shared_access_key_enabled           = var.shared_access_key_enabled
+  public_network_access_enabled       = var.public_network_access_enabled
+  default_to_oauth_authentication     = var.default_to_oauth_authentication
+  queue_encryption_key_type           = var.queue_encryption_key_type
+  table_encryption_key_type           = var.table_encryption_key_type
+  infrastructure_encryption_enabled   = var.infrastructure_encryption_enabled
+  
+  dynamic "blob_properties" {
+    for_each = var.account_kind == "BlobStorage" ? [1] : []
+    content {
+      versioning_enabled = var.versioning_enabled
+      change_feed_enabled = var.change_feed_enabled
+      last_access_time_enabled = var.last_access_time_enabled
+      delete_retention_policy {
+        days = 7
+      }
+      container_delete_retention_policy {
+        days = 7
+      }
 
+      
+    }
+
+  }
   lifecycle {
     ignore_changes = [
       tags,
--- variables.tf
+++ variables.tf
@@ -1,59 +1,118 @@
 variable "name" {
   type        = string
   description = "value"
+
 }
 variable "resource_group_name" {
   type        = string
   description = "value"
+
 }
 variable "location" {
   type        = string
   description = "value"
-}
-variable "account_kind" {
-  type        = string
-  description = "value"
+
 }
 variable "account_tier" {
   type        = string
   description = "value"
-  default     = "Premium"
+  default     = "Standard"
+
+}
+variable "account_kind" {
+  description = "value"
+  type        = string
+  # default     = "BlobStorage"
+
 }
 variable "account_replication_type" {
   type        = string
   description = "value"
-}
-variable "min_tls_version" {
-  type        = string
-  description = "value"
-  default     = "TLS1_2"
+  default     = "LRS"
+
 }
 variable "cross_tenant_replication_enabled" {
   type        = bool
   description = "value"
   default     = true
+
 }
-variable "allow_nested_items_to_be_public" {
-  type        = bool
-  description = "value"
-  default     = true
-}
-variable "shared_access_key_enabled" {
-  type        = bool
-  description = "value"
-  default     = true
-}
-variable "public_network_access_enabled" {
-  type        = bool
-  description = "value"
-  default     = true
-}
-variable "enable_https_traffic_only" {
-  type = bool
-  default = false
-}
-variable "type" {
+variable "access_tier" {
   type        = string
   description = "value"
-  default     = "SystemAssigned"
+  default     = "Hot"
+
+}
+variable "enable_https_traffic_only" {
+  description = "value"
+  type        = bool
+  default     = true
+
+}
+variable "min_tls_version" {
+  type        = string
+  description = "value"
+  default     = "TLS1_2"
+
+}
+variable "allow_nested_items_to_be_public" {
+  description = "value"
+  type        = bool
+  default     = true
+
+}
+variable "shared_access_key_enabled" {
+  description = "value"
+  type        = bool
+  default     = true
+
+}
+variable "public_network_access_enabled" {
+  description = "value"
+  type        = bool
+  default     = true
+
+}
+variable "default_to_oauth_authentication" {
+  description = "value"
+  type        = bool
+  default     = false
+
+}
+variable "queue_encryption_key_type" {
+  type        = string
+  description = "value"
+  default     = "Service"
+
+}
+variable "table_encryption_key_type" {
+  type        = string
+  description = "value"
+  default     = "Service"
+
+}
+variable "infrastructure_encryption_enabled" {
+  description = "value"
+  type        = bool
+  default     = false
+
+}
+
+variable "versioning_enabled" {
+    type = bool
+    description = "value"
+    default     = false
+
+}
+variable "change_feed_enabled" {
+    type = bool
+    description = "value"
+    default     = false
+
+}
+variable "last_access_time_enabled" {
+    type = bool
+    description = "value"
+    default     = false
+
 }
