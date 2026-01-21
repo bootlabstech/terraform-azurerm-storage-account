8462912412c239aac111c2a5d0161f9c7b7d9a75
lalithaa2407 <lalitalallu02@gmail.com>
2026-01-21 17:53:52 +0530

    updated module provider version >4.0.0 along with private endpoint

--- main.tf
+++ main.tf
@@ -46,3 +46,28 @@ resource "azurerm_storage_account" "example" {
   }
 
 }
+resource "azurerm_private_endpoint" "endpoint" {
+  name                = "${var.name}-pe"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  subnet_id           = var.private_endpoint_subnet_id
+
+  private_service_connection {
+    name                           = "${var.name}-connection"
+    private_connection_resource_id = var.private_connection_resource_id
+    is_manual_connection           = var.is_manual_connection
+    subresource_names              = var.subresource_names
+
+  }
+  private_dns_zone_group {
+    name                 = "${var.name}-dnszone"
+    private_dns_zone_ids = var.private_dns_zone_ids
+  }
+  lifecycle {
+    ignore_changes = [
+      tags,
+    ]
+  }
+  depends_on = [ azurerm_storage_account.example ]
+
+}
--- variables.tf
+++ variables.tf
@@ -121,3 +121,39 @@ variable "is_hns_enabled" {
   type = bool
   
 }
+
+
+# PE
+# private endpoint
+
+variable "private_endpoint_subnet_id" {
+  type        = string
+  description = "The ID of the Subnet from which Private IP Addresses will be allocated for this Private Endpoint."
+
+}
+variable "private_connection_resource_id" {
+  type        = string
+  description = "The ID of the Private Link Enabled Remote Resource which this Private Endpoint should be connected to."
+
+}
+variable "is_manual_connection" {
+  type        = bool
+  description = "Does the Private Endpoint require Manual Approval from the remote resource owner?"
+  default     = false
+
+}
+variable "subresource_names" {
+  type        = list(string)
+  description = " A list of subresource names which the Private Endpoint is able to connect to."
+  default = [ "blob" ]
+}
+variable "private_dns_zone_ids" {
+  type        = list(string)
+  description = "Specifies the list of Private DNS Zones to include within the private_dns_zone_group."
+}
+
+variable "ip_configuration" {
+  type        = bool
+  default     = false
+  description = "One or more ip_configuration blocks as defined below. This allows a static IP address to be set for this Private Endpoint, otherwise an address is dynamically allocated from the Subnet."
+}
